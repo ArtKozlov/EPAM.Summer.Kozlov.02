905bfd3816524b0767decc023786f7ebadf423af
Artsiom <Artsiomxxx@gmail.com>
2016-07-03 01:23:40 +0300

    add runtime for Stein algorithm

--- GCD/SteinAlgorithm.cs
+++ GCD/SteinAlgorithm.cs
@@ -1,35 +1,84 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace GCD
 {
     public class SteinAlgorithm
     {
-            public int GetGCD(int firstNumber, int secondNumber)
-            {
 
-                if (firstNumber == 0 || secondNumber == 0) throw new Exception("Недопустимые аргументы!");
-                if (firstNumber == secondNumber) return firstNumber;
-                if (firstNumber == 1 || secondNumber == 1) return 1;
-                if ((firstNumber % 2 == 0) && (secondNumber % 2 == 0)) return 2 * GetGCD(firstNumber / 2, secondNumber / 2);
-                if ((firstNumber % 2 == 0) && (secondNumber % 2 != 0)) return GetGCD(firstNumber / 2, secondNumber);
-                if ((firstNumber % 2 != 0) && (secondNumber % 2 == 0)) return GetGCD(firstNumber, secondNumber / 2);
-                return GetGCD(secondNumber, Math.Abs(firstNumber - secondNumber));
-            }
-
-            public int GetGCD(int firstNumber, int secondNumber, int thirdNumber)
+        Stopwatch stopWatch = new Stopwatch();
+        private long runTime;
+        public long RunTime
+        {
+            get
             {
-                firstNumber = GetGCD(firstNumber, secondNumber);
-                return GetGCD(firstNumber, thirdNumber);
+                return runTime;
             }
+        }
 
-            public int GetGCD(params int[] arrayOfNumbers)
+        public int GetGCD(int firstNumber, int secondNumber)
+        {
+            stopWatch.Start();
+            runTime = 0;
+            return GCD(firstNumber, secondNumber);
+
+        }
+
+        public int GetGCD(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            stopWatch.Start();
+            runTime = 0;
+            firstNumber = GCD(firstNumber, secondNumber);
+            return GCD(firstNumber, thirdNumber);
+        }
+
+        public int GetGCD(params int[] arrayOfNumbers)
+        {
+            stopWatch.Start();
+            runTime = 0;
+            int result = arrayOfNumbers[0];
+            for (int i = 0; i < arrayOfNumbers.Length - 1; i++)
             {
-                int result = arrayOfNumbers[0];
-                for (int i = 0; i < arrayOfNumbers.Length - 1; i++)
-                {
-                    result = GetGCD(result, arrayOfNumbers[i++]);
-                }
-                return result;
+                result = GCD(result, arrayOfNumbers[i++]);
             }
-      }
+            return result;
+        }
+
+        public int GCD(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == 0 || secondNumber == 0) throw new Exception("Недопустимые аргументы!");
+            if (firstNumber == secondNumber)
+            {
+                SaveRunTime();
+                return firstNumber;
+            }
+            if (firstNumber == 1 || secondNumber == 1)
+            {
+                SaveRunTime();
+                return 1;
+            }
+            if ((firstNumber % 2 == 0) && (secondNumber % 2 == 0))
+            {
+                return 2 * GCD(firstNumber / 2, secondNumber / 2);
+            }
+            if ((firstNumber % 2 == 0) && (secondNumber % 2 != 0))
+            {
+                return GCD(firstNumber / 2, secondNumber);
+            }
+            if ((firstNumber % 2 != 0) && (secondNumber % 2 == 0))
+            {
+                return GCD(firstNumber, secondNumber / 2);
+            }
+            return GCD(secondNumber, Math.Abs(firstNumber - secondNumber));
+        }
+
+        private void SaveRunTime()
+        {
+            stopWatch.Stop();
+            runTime += stopWatch.ElapsedTicks;
+            stopWatch.Reset();
+        }
+
+
+    }
 }
--- UI/Program.cs
+++ UI/Program.cs
@@ -13,13 +13,8 @@ namespace UI
             Console.WriteLine(obj.GetGCD(3, 17));
             Console.WriteLine(obj.GetGCD(78, 294,570));
             Console.WriteLine(obj.GetGCD(78, 294, 570, 36));
-              Console.WriteLine("Нахождение корня методом Ньютона:");
-              double root = MathNewton.Sqrt(100, 3, 0.00000001);
-              Console.WriteLine(root);
-              Console.WriteLine("Проверка:");
-              Console.WriteLine(Math.Pow(root, 2));   */
-
-            EuclideanAlgorithm obj = new EuclideanAlgorithm();
+            
+            SteinAlgorithm obj = new SteinAlgorithm();
             Console.WriteLine(obj.GetGCD(3, 17));
             Console.WriteLine("Runtime in Ticks: " + obj.RunTime);
             Console.WriteLine(obj.GetGCD(135, 972));
@@ -28,7 +23,13 @@ namespace UI
             Console.WriteLine("Runtime in Ticks: " + obj.RunTime);
             Console.WriteLine(obj.GetGCD(78, 294, 570, 36));
             Console.WriteLine("Runtime in Ticks: " + obj.RunTime);
+            */
 
+            Console.WriteLine("Нахождение корня методом Ньютона:");
+            double root = MathNewton.Sqrt(100, 3, 0.00000001);
+            Console.WriteLine(root);
+            Console.WriteLine("Проверка:");
+            Console.WriteLine(Math.Pow(root, 2));   
 
             Console.ReadKey();
 
