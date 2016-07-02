fe0a12d9261ca6c0eb3863c7b1dd99895ecc0516
Artsiom <Artsiomxxx@gmail.com>
2016-07-02 17:03:23 +0300

    -

---
+++ UI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LogicNewton;
+using GCD;
+
+namespace UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Нахождение корня методом Ньютона:");
+            double root = MathNewton.Sqrt(2, 2, 0.00000000001);
+            Console.WriteLine(root);
+            Console.WriteLine("Проверка:");
+            Console.WriteLine(Math.Pow(root, 2));
+
+            Console.ReadKey();
+
+        }
+    }
+}
