ebd43c2cbaa26337fcf14d56fbf78e9ce807d208
Artsiom <Artsiomxxx@gmail.com>
2016-07-03 01:08:28 +0300

    -

--- GCD/EuclideanAlgorithm .cs
+++ GCD/EuclideanAlgorithm .cs
@@ -1,58 +1,83 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Threading;
 
 namespace GCD
 {
     public class EuclideanAlgorithm
     {
-      /*  Stopwatch stopWatch = new Stopwatch();
-        private string runTime = null;
-        public string RunTime
+
+        Stopwatch stopWatch = new Stopwatch();
+        private long runTime;
+        public long RunTime
         {
             get
             {
                 return runTime;
             }
-        }*/
+        }
         public int GetGCD(int firstNumber, int secondNumber)
         {
-            /*  if (stopWatch.ElapsedMilliseconds == 0)
-              {
-                  stopWatch.Start();
-              } */
-            if (firstNumber == secondNumber) return firstNumber;
-            if (firstNumber == 1 || secondNumber == 1) return 1;
-            if (firstNumber == 0 || secondNumber == 0)
-            {
-                throw new Exception("Недопустимые аргументы!");
-            }
-                if (firstNumber%secondNumber != 0)
-            {
-                return GetGCD(secondNumber, firstNumber%secondNumber);
-            }
+            stopWatch.Start();
+            runTime = 0;
+            return GCD(firstNumber, secondNumber);
 
-           /* stopWatch.Stop();
-            runTime = "Runtime in Milliseconds: " + Convert.ToString(stopWatch.ElapsedMilliseconds);*/
-
-            return Math.Abs(secondNumber);
         }
 
         public int GetGCD(int firstNumber, int secondNumber, int thirdNumber)
         {
-            firstNumber = GetGCD(firstNumber, secondNumber);
-            return GetGCD(firstNumber, thirdNumber);
+            stopWatch.Start();
+            runTime = 0;
+            firstNumber = GCD(firstNumber, secondNumber);
+            return GCD(firstNumber, thirdNumber);
         }
 
         public int GetGCD(params int[]arrayOfNumbers)
         {
+            stopWatch.Start();
+            runTime = 0;
             int result = arrayOfNumbers[0];
             for (int i = 0; i < arrayOfNumbers.Length - 1; i++)
             {
-                result = GetGCD(result, arrayOfNumbers[i++]);
+                result = GCD(result, arrayOfNumbers[i++]);
             }
             return result;
         }
-    }
+
+        public int GCD(int firstNumber, int secondNumber)
+        {
+
+            if (firstNumber == secondNumber)
+            {
+                SaveRunTime();
+                return firstNumber;
+            }
+            if (firstNumber == 1 || secondNumber == 1)
+            {
+                SaveRunTime();
+                return 1;
+            }
+
+            if (firstNumber == 0 || secondNumber == 0)
+            {
+                throw new Exception("Недопустимые аргументы!");
+            }
+
+            if (firstNumber % secondNumber != 0)
+            {
+                return GCD(secondNumber, firstNumber % secondNumber);
+            }
+
+            SaveRunTime();
+            return Math.Abs(secondNumber);
+        }
+
+        private void SaveRunTime()
+        {
+            stopWatch.Stop();
+            runTime += stopWatch.ElapsedTicks;
+            stopWatch.Reset();
+        }
+
+        }
 
 }
